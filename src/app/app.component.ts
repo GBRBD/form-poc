511338aa9f4fc8d947ce63f6e3c471cd9fbda6fa
Gabor Bodi <gabor.bodi@zf.com>
2022-04-22 16:08:11 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,60 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  selectedProtocol: FormGroup | null = null
 
   form = this.fb.group({
-    select:[null,  [Validators.required]],
+    select: [null, [Validators.required]],
+    protocols: this.fb.array([
+      this.fb.group({
+        id: 'basic',
+        name: [null, [Validators.required]]
+      }),
+      this.fb.group({
+        id: 'address',
+        city: [null, [Validators.required]]
+      })
+    ]),
   });
 
-  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
-    this.form.valueChanges.subscribe(res => {
-      this.cdr.detectChanges()
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.disableProtocols();
+    this.selectControl?.valueChanges.subscribe(() => {
+      this.disableProtocols()
+      this.selectedProtocol = this.protocolsControl?.controls.find(x => x.value.id === this.selectControl?.value) as FormGroup;
+      this.selectedProtocol.enable()
+    })
+  }
+
+  private disableProtocols(): void {
+    this.protocolsControl?.controls.forEach(x => {
+      x.disable()
     })
   }
 
   onSubmit() {
     console.log(this.form.value)
   }
+
   getForm() {
     console.log(this.form)
     console.log(this.form.valid)
   }
 
   get selectControl() {
-   return this.form.get('select')
+    return this.form.get('select')
   }
 
+  get protocolsControl(): FormArray | null {
+    return this.form.get('protocols') as FormArray;
+  }
 }
--- src/app/basic-info/basic-info.component.ts
+++ src/app/basic-info/basic-info.component.ts
@@ -1,29 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
+import {Component, Input} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+
 
 @Component({
   selector: 'app-basic-info',
   templateUrl: './basic-info.component.html',
   styleUrls: ['./basic-info.component.scss']
 })
-export class BasicInfoComponent implements OnInit {
-  @Input() formGroupName!: string;
-
-  form!: FormGroup;
-
-  constructor(private rootFormGroup: FormGroupDirective, private fb: FormBuilder) {
-  }
-
-  ngOnInit(): void {
-    const group = this.fb.group({
-      name: [null, [Validators.required]]
-    })
-    this.rootFormGroup.form.addControl("basic", group)
-    this.form = this.rootFormGroup.control.get(this.formGroupName) as FormGroup;
-  }
-
-  ngOnDestroy(): void {
-    this.rootFormGroup.form.removeControl(this.formGroupName)
-  }
-
+export class BasicInfoComponent {
+  @Input() form!: FormGroup;
 }
